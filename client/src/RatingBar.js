58e586df3aa8323f2ce3c47836f1a21c210b04b0
BEN1JEN <benpheard@gmail.com>
2020-05-30 17:02:48 -0600

    Improve gui

--- client/src/RatingBar.js
+++ client/src/RatingBar.js
@@ -3,7 +3,13 @@ import './RatingBar.css';
 
 function RatingBar(props) {
   return (
-    <div className="RatingBar" onClick={props.userRate}>
+    <div className="RatingBar">
+      <span>How Safe do you feel in this store?</span>
+      <div className="RatingBar_gradient" onClick={props.userRate}>
+        <p className="RatingBar_marker RatingBar_marker-safe">safe</p>
+        <p className="RatingBar_marker RatingBar_marker-ok">OK</p>
+        <p className="RatingBar_marker RatingBar_marker-unsafe">unsafe</p>
+      </div>
     </div>
   );
 }
