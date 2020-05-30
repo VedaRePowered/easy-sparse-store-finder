08e36eec3939298c7e17a71d16b33cff70e718f8
BEN1JEN <benpheard@gmail.com>
2020-05-29 22:40:36 -0600

    Add cordova TODO: remove build files

---
+++ client/platforms/android/app/src/main/assets/www/precache-manifest.58e7c7f907c1b80df54ef2263d4f3016.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "103886ab281cb778914cddc79702fd9d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "01c28bbab54120750947",
+    "url": "./static/css/main.935a9a24.chunk.css"
+  },
+  {
+    "revision": "94b86991514255dacbdf",
+    "url": "./static/js/2.3e13facd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.3e13facd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01c28bbab54120750947",
+    "url": "./static/js/main.cbb08048.chunk.js"
+  },
+  {
+    "revision": "cdc93521bb7c93186e96",
+    "url": "./static/js/runtime-main.6c87e859.js"
+  },
+  {
+    "revision": "128f7fdb41dc80d1eb977efdb77da99c",
+    "url": "./static/media/logo.128f7fdb.svg"
+  }
+]);
