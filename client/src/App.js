17cfa30be9cb99d921f63f7799eed0693deb3570
BEN1JEN <benpheard@gmail.com>
2020-05-30 14:07:41 -0600

    Add probably working geolocation code

--- client/src/App.js
+++ client/src/App.js
@@ -3,39 +3,39 @@ import logo from './logo.svg';
 import './App.css';
 import ListView from './ListView';
 
-function App() {
-  const stores = [
-    {
-      "name": "Safeway",
-      "userRating": 4
-    },
-    {
-      "name": "Costco",
-      "userRating": 0
-    },
-    {
-      "name": "Sobeys",
-      "userRating": 3
-    },
-    {
-      "name": "Superstore",
-      "userRating": 1
-    },
-    {
-      "name": "Co-op",
-      "userRating": 2
-    },
-  ]
-  return (
-    <div className="App">
-      <header>
-        <p>
-          Header
-        </p>
-      </header>
-      <ListView stores={stores} />
-    </div>
-  );
+const socket = new WebSocket("ws://127.0.0.1:12345");
+class App extends React.Component {
+  constructor(props, context) {
+    super(props, context)
+    this.state = {
+      stores: [],
+    };
+  }
+  componentDidMount() {
+    socket.onopen = function(e) {
+      navigator.geolocation.getCurrentPosition(
+        position => socket.send("getRatings," + position.coords.latitude.toString() + "," + position.coords.longitude.toString()),
+        error => alert(error.message)
+      );
+      socket.send("test");
+    };
+    socket.onmessage = function(event) {
+      alert(`[message] Data received from server: ${event.data}`);
+    };
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header>
+          <p>
+            Header
+          </p>
+        </header>
+        <ListView stores={this.state.stores} />
+      </div>
+    );
+  }
 }
 
 export default App;
